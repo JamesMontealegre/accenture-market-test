6d9dad1300c10784d59d01b4e3ffb49b59f879de
JamesMontealegre <js.montealegre@uniandes.edu.co>
2025-01-30 14:19:05 -0500

    feat(*): improve data mappings

--- src/main/java/com/accenture_challenge/persistance/mapper/BranchMapper.java
+++ src/main/java/com/accenture_challenge/persistance/mapper/BranchMapper.java
@@ -4,22 +4,18 @@ import com.accenture_challenge.domain.dto.BranchDTO;
 import com.accenture_challenge.persistance.entity.Branch;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface BranchMapper {
 
-    @Mappings({
-            @Mapping(source = "franchise.id", target = "franchiseId"),
-            @Mapping(source = "franchise.name", target = "franchiseName")
-    })
+    @Mapping(source = "franchise.id", target = "franchiseId")
+    @Mapping(source = "franchise.name", target = "franchiseName")
     BranchDTO toDTO(Branch branch);
 
-    @Mappings({
-            @Mapping(target = "franchise", ignore = true)
-    })
+    @Mapping(target = "franchise", ignore = true)
+    @Mapping(target = "products", ignore = true)
     Branch toEntity(BranchDTO branchDTO);
 
     List<BranchDTO> toDTOList(List<Branch> branches);
--- src/main/java/com/accenture_challenge/persistance/mapper/ProductMapper.java
+++ src/main/java/com/accenture_challenge/persistance/mapper/ProductMapper.java
@@ -4,14 +4,14 @@ import com.accenture_challenge.domain.dto.ProductDTO;
 import com.accenture_challenge.persistance.entity.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-    @Mappings({@Mapping(source = "branch.id", target = "branchId"), @Mapping(source = "branch.name", target = "branchName")})
+    @Mapping(source = "branch.id", target = "branchId")
+    @Mapping(source = "branch.name", target = "branchName")
     ProductDTO toDTO(Product product);
 
     List<ProductDTO> toDTOList(List<Product> products);
